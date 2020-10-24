7fcf05c81a1fb391b4a75da41b3068c8444fd744
_Rabbit_ <martynov-danil@list.ru>
2020-10-24 15:14:13 +0500

    Updates

---
+++ precache-manifest.d9523517d4f06b89a4267cde5702c7a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b94c4bde507ffc35bfc4f72554b16f6",
+    "url": "/TestJuniorDeveloper/index.html"
+  },
+  {
+    "revision": "1f0473a1b1e5de215034",
+    "url": "/TestJuniorDeveloper/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "997bf3c30e042ab64c21",
+    "url": "/TestJuniorDeveloper/static/js/2.2fa89b78.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/TestJuniorDeveloper/static/js/2.2fa89b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0473a1b1e5de215034",
+    "url": "/TestJuniorDeveloper/static/js/main.e47b042b.chunk.js"
+  },
+  {
+    "revision": "c98ff2672b89026a8132",
+    "url": "/TestJuniorDeveloper/static/js/runtime-main.72817100.js"
+  }
+]);
